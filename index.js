c4029c62358c087b516b0e4c2a6a2b69725cd989
Mike Shine <michaelbshine@outlook.com>
2021-01-24 13:28:52 -0800

    added running score and win feature to DOM

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const body = document.querySelector('#body');
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+
+const div = document.createElement('div');
+div.style.backgroundColor = 'green';
+div.style.border = '1px dotted black';
+
+const h3 = document.createElement('h3');
+h3.textContent = `Player Score: ${playerScore}       Computer Score: ${computerScore}`;
+h3.style.color = 'white'
+
+div.appendChild(h3);
+body.appendChild(div);
+
+
+
+
 function computerPlay() {
   const options = ['ROCK', 'PAPER', 'SCISSORS'];
   let selection = options[Math.floor(Math.random() * 3)]
@@ -6,66 +27,70 @@ function computerPlay() {
 
 
 function game() {
-  let playerScore = 0;
-  let computerScore = 0;
+
 
   function playRound(playerSelection, computerSelection) {
   if (playerSelection.toUpperCase() === computerSelection) {
-    return 'It\'s a draw! Play another round.'
+    h3.textContent=`It\'s a draw! Play another round. The current score is - Player: ${playerScore} and Computer: ${computerScore}.`
   } else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection === 'PAPER') {
     computerScore++;
-    return 'Computer wins! Paper beats rock.'
+    h3.textContent=`Computer wins! Paper beats rock.The current score is - Player: ${playerScore} and Computer: ${computerScore}.`
   } else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection === 'SCISSORS') {
     playerScore++;
-    return 'Player wins! Rock beats scissors.'
+    h3.textContent=`Player wins! Rock beats scissors.The current score is - Player: ${playerScore} and Computer: ${computerScore}.`
   } else if (playerSelection.toUpperCase() === 'PAPER' && computerSelection === 'ROCK') {
     playerScore++;
-    return 'Player wins! Paper beats rock.'
+    h3.textContent=`Player wins! Paper beats rock.The current score is - Player: ${playerScore} and Computer: ${computerScore}.`
   } else if (playerSelection.toUpperCase() === 'PAPER' && computerSelection === 'SCISSORS') {
     computerScore++;
-    return 'Computer wins! Scissors beats paper.'
+    h3.textContent=`Computer wins! Scissors beats paper. The current score is - Player: ${playerScore} and Computer: ${computerScore}.`
   } else if (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection === 'ROCK') {
     computerScore++;
-    return 'Computer wins! Scissors beats rock.'
+    h3.textContent=`Computer wins! Scissors beats rock. The current score is - Player: ${playerScore} and Computer: ${computerScore}.`
   } else if (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection === 'PAPER') {
     playerScore++;
-    return 'Player wins! Scissors beats paper.'
+    h3.textContent=`Player wins! Scissors beats paper. The current score is - Player: ${playerScore} and Computer: ${computerScore}.`
+  }
+
+  if (playerScore === 5) {
+    setTimeout(() => {
+      alert('Congratulations, you beat the computer!')
+    }, 100)
+    playerScore = 0;
+    computerScore = 0;
+  }
+  if (computerScore === 5) {
+    setTimeout(() => {
+      alert('Ooof, you just got beat by the computer. Better luck next time!')
+    }, 100)
+    playerScore = 0;
+    computerScore = 0;
   }
 }
 
-  // for (let i = 1; i <= 5; i++) {
-  //   let playerOption = prompt('Enter rock or paper or scissors: ')
-  //   let computerOption = computerPlay();
-  //   playRound(playerOption, computerOption);
-  //   console.log(`The current score is - Player:${playerScore} and Computer:${computerScore}.`)
-  // }
-
   const rockButton = document.querySelector('#rock');
   rockButton.addEventListener('click', () => {
-    alert('you picked rock!')
+    playRound('ROCK', computerPlay());
+    console.log(`The current score is - Player:${playerScore} and Computer:${computerScore}.`)
   })
 
   const paperButton = document.querySelector('#paper');
   paperButton.addEventListener('click', () => {
-    alert('you picked paper!')
+    playRound('PAPER', computerPlay());
+    console.log(`The current score is - Player:${playerScore} and Computer:${computerScore}.`)
   })
 
   const scissorsButton = document.querySelector('#scissors');
   scissorsButton.addEventListener('click', () => {
-    alert('you picked scissors!')
+    playRound('SCISSORS', computerPlay());
+    console.log(`The current score is - Player:${playerScore} and Computer:${computerScore}.`)
   })
 
   // when a button is clicked, play a round
   // display running score
   // when someone reaches five points, declare a winner
 
-  if (playerScore > computerScore) {
-    console.log('Congratulations, you beat the computer!')
-  } else if (playerScore < computerScore) {
-    console.log('Ooof, you just got beat by the computer. Better luck next time!')
-  } else {
-    console.log('How interesting, a tie game! Nicely done.')
-  }
+
 
 }
 
